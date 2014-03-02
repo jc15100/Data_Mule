59adc71c02174d9d9d41bc96becfcd6d13c55e4f
juancgarcia <jgarcia39@gatech.edu>
2014-03-01 19:04:11 -0500

    Including simple arduino interface

--- arduino_interface/AdHocInterface.cpp
+++ arduino_interface/AdHocInterface.cpp
@@ -1,11 +1,33 @@
 #include "AdHocInterface.h"
-#include <iostream.h>
+#include <sys/time.h>
 
-void Session::open(std::string id, int size){
-	std::stringstream filepath = "~/data_" << id;
-	
-	file = fopen(filepath, "w+");
+using namespace std;
 
+void Session::open(std::string sensorname){
+    timeval current;
+    gettimeofday(&current, 0);
 
+    std::stringstream filename;
+    std::string datafile;
+    filename << sensorname << (current.tv_sec + current.tv_usec*10e6);
+    filename >> datafile;
+
+    file.open(filename);
+    isOpen = true;
 }
+
+void Session::write(float data){
+    file << data << endl;
+}
+
+void Session::close(){
+    file.close();
+    isOpen = false;
+}
+
+void Session::send(){
+    // call Andrew's comm API
+}
+
+
 
