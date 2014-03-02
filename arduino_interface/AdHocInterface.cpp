61c87aaf32f2733745a90a95ba2c72dfa4d48599
juancgarcia <jgarcia39@gatech.edu>
2014-03-01 19:21:25 -0500

    Fixed stupid mistakes

--- arduino_interface/AdHocInterface.cpp
+++ arduino_interface/AdHocInterface.cpp
@@ -1,20 +1,23 @@
 #include "AdHocInterface.h"
 #include <sys/time.h>
+#include <sstream>
 
 using namespace std;
 
-void Session::open(std::string sensorname){
+void Session::open(string sensorname){
     timeval current;
     gettimeofday(&current, 0);
 
-    std::stringstream filename;
-    std::string datafile;
+    stringstream filename;
+    string datafile;
     filename << sensorname << (current.tv_sec + current.tv_usec*10e6);
     filename >> datafile;
 
-    file.open(filename);
+    file.open(filename.str().c_str(), std::ofstream::out | std::ofstream::app);
     isOpen = true;
 }
+
+Session::~Session(){}
 
 void Session::write(float data){
     file << data << endl;
@@ -27,6 +30,14 @@ void Session::close(){
 
 void Session::send(){
     // call Andrew's comm API
+}
+
+int main(){
+    Session test;
+    test.open("temperature");
+    test.write(4.56);
+    test.close();
+    return 0;
 }
 
 
