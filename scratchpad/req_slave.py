e87874675da94713f2146b890d5ecb1f42635081
tuxerman <spzone@gmail.com>
2014-03-01 20:59:46 -0500

    ad hoc server client

--- scratchpad/req_slave.py
+++ scratchpad/req_slave.py
@@ -1,12 +1,13 @@
 import socket
 import time
 
-host = '192.168.1.2' #vinay's GT IP
-port = 5002
+#host = '192.168.1.3' #vinay's GT IP
+host = ''
+port = 5052
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
-resp = s.send(1)
+resp = s.send('1')
 print resp
-resp = s.send(3)
+resp = s.send('3')
 print resp
